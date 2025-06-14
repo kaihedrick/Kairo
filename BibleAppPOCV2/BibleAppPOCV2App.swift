ebb3d43f0315ab271345664d39463a7255b0e697
Karsten Hedrick <KHedrick5@my.gcu.edu>
2025-06-13 21:51:37 -0700

    Initial Commit

---
+++ BibleAppPOCV2/BibleAppPOCV2App.swift
@@ -0,0 +1,17 @@
+//
+//  BibleAppPOCV2App.swift
+//  BibleAppPOCV2
+//
+//  Created by Jeff Hedrick on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct BibleAppPOCV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BibleAppPOCV2Tests/BibleAppPOCV2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  BibleAppPOCV2Tests.swift
+//  BibleAppPOCV2Tests
+//
+//  Created by Jeff Hedrick on 6/13/25.
+//
+
+import Testing
+@testable import BibleAppPOCV2
+
+struct BibleAppPOCV2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
